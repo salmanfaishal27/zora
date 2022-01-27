e665892e9729e66305a2628f7078d1853434503c
salmanfaishal27 <salmanfaishal27@gmail.com>
2022-01-28 03:20:04 +0700

    next 9

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -2,6 +2,8 @@
 
 @section('container')
     <h1 class="mb-5">{{ $post->title }}</h1>
+
+    <p>By. Salmanfaishal in <a href="/categories/{{ $post->category->slug }}"> {{ $post->category->name }}</a></p>
     {!! $post->body !!}
 
     <a href="/posts">Back to Post</a>
