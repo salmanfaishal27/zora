3330b842a51bd8b9800b084f41efeacf7b6942b6
salmanfaishal27 <salmanfaishal27@gmail.com>
2022-02-09 19:30:40 +0700

    next 11

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,18 +16,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-
-        User::create([
-            'name'=> 'Salman Faishal',
-            'email'=>'salmanfaishal@gmail.com',
-            'password'=> bcrypt('12345')
-        ]);
-        User::create([
-            'name'=> 'Jason',
-            'email'=>'jason@gmail.com',
-            'password'=> bcrypt('12345')
-        ]);
+        
+        User::factory(3)->create();
 
         Category::create([
             'name'=>'Programming',
@@ -38,40 +28,8 @@ class DatabaseSeeder extends Seeder
             'slug'=>'personal',            
         ]);
 
-        Post::create([
-            'title'=>'Judul Pertama',
-            'slug'=>'judul-pertama',
-            'excerpt'=>'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam perspiciatis aliquam autem quos quidem nulla, perferendis similique, sed sequi consequuntur dolorem? Quas suscipit impedit',
-            'body'=>'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam perspiciatis aliquam autem quos quidem nulla, perferendis similique, sed sequi consequuntur dolorem? Quas suscipit impedit, quod reiciendis doloremque voluptas magni labore illo velit minima porro veniam delectus! Pariatur in odio quis modi corrupti natus ipsam dignissimos veniam exercitationem, nisi alias qui, facere obcaecati quam voluptate culpa temporibus repellendus labore necessitatibus vero! Porro dolore, consequuntur reprehenderit impedit fugiat fugit minima nesciunt excepturi accusamus aliquid maiores voluptatum. Harum sequi iusto natus, recusandae a dolores itaque assumenda minus alias enim qui necessitatibus perspiciatis cupiditate fuga deleniti consequuntur dicta praesentium nemo expedita, sint ea ut.',
-            'category_id'=>1,
-            'user_id'=>1
-        ]);
+        Post::factory(20)->create();
 
-        Post::create([
-            'title'=>'Judul Kedua',
-            'slug'=>'judul-kedua',
-            'excerpt'=>'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam perspiciatis aliquam autem quos quidem nulla, perferendis similique, sed sequi consequuntur dolorem? Quas suscipit impedit',
-            'body'=>'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam perspiciatis aliquam autem quos quidem nulla, perferendis similique, sed sequi consequuntur dolorem? Quas suscipit impedit, quod reiciendis doloremque voluptas magni labore illo velit minima porro veniam delectus! Pariatur in odio quis modi corrupti natus ipsam dignissimos veniam exercitationem, nisi alias qui, facere obcaecati quam voluptate culpa temporibus repellendus labore necessitatibus vero! Porro dolore, consequuntur reprehenderit impedit fugiat fugit minima nesciunt excepturi accusamus aliquid maiores voluptatum. Harum sequi iusto natus, recusandae a dolores itaque assumenda minus alias enim qui necessitatibus perspiciatis cupiditate fuga deleniti consequuntur dicta praesentium nemo expedita, sint ea ut.',
-            'category_id'=>1,
-            'user_id'=>1
-        ]);
-
-        Post::create([
-            'title'=>'Judul Ketiga',
-            'slug'=>'judul-ketiga',
-            'excerpt'=>'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam perspiciatis aliquam autem quos quidem nulla, perferendis similique, sed sequi consequuntur dolorem? Quas suscipit impedit',
-            'body'=>'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam perspiciatis aliquam autem quos quidem nulla, perferendis similique, sed sequi consequuntur dolorem? Quas suscipit impedit, quod reiciendis doloremque voluptas magni labore illo velit minima porro veniam delectus! Pariatur in odio quis modi corrupti natus ipsam dignissimos veniam exercitationem, nisi alias qui, facere obcaecati quam voluptate culpa temporibus repellendus labore necessitatibus vero! Porro dolore, consequuntur reprehenderit impedit fugiat fugit minima nesciunt excepturi accusamus aliquid maiores voluptatum. Harum sequi iusto natus, recusandae a dolores itaque assumenda minus alias enim qui necessitatibus perspiciatis cupiditate fuga deleniti consequuntur dicta praesentium nemo expedita, sint ea ut.',
-            'category_id'=>2,
-            'user_id'=>1
-        ]);
-
-        Post::create([
-            'title'=>'Judul Empat',
-            'slug'=>'judul-empat',
-            'excerpt'=>'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam perspiciatis aliquam autem quos quidem nulla, perferendis similique, sed sequi consequuntur dolorem? Quas suscipit impedit',
-            'body'=>'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam perspiciatis aliquam autem quos quidem nulla, perferendis similique, sed sequi consequuntur dolorem? Quas suscipit impedit, quod reiciendis doloremque voluptas magni labore illo velit minima porro veniam delectus! Pariatur in odio quis modi corrupti natus ipsam dignissimos veniam exercitationem, nisi alias qui, facere obcaecati quam voluptate culpa temporibus repellendus labore necessitatibus vero! Porro dolore, consequuntur reprehenderit impedit fugiat fugit minima nesciunt excepturi accusamus aliquid maiores voluptatum. Harum sequi iusto natus, recusandae a dolores itaque assumenda minus alias enim qui necessitatibus perspiciatis cupiditate fuga deleniti consequuntur dicta praesentium nemo expedita, sint ea ut.',
-            'category_id'=>2,
-            'user_id'=>2
-        ]);
+       
     }
 }
